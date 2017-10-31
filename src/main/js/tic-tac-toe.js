1b02341a8c3b4f3a19d98a8aa5826d2f69f5554f
Bretton Wade <bretton.wade@gmail.com>
2017-10-31 12:08:13 -0400

    enums and players, first implementation test code for enums and players

--- src/main/js/tic-tac-toe.js
+++ src/main/js/tic-tac-toe.js
@@ -13,3 +13,60 @@ let Base = function () {
 	
 	return _;
 } ();
+
+
+let Enum = function () {
+    let _ = Object.create (null);
+    _.create = function (names) {
+        let createdEnum = Object.create (null);
+
+        // function to create an enumerated object
+        let make = function (name, value) {
+            //console.log ("enumerate '" + name + "' = " + value);
+            let enumeratedValue = Object.create (createdEnum);
+            Object.defineProperty(enumeratedValue, "name", { value: name });
+            Object.defineProperty(enumeratedValue, "value", { value: value });
+            return enumeratedValue;
+        };
+
+        // create the enumerated values, which are Objects of this type already populated
+        let enumeratedValues = [];
+        for (let name of names) {
+            let enumeratedValue = make (name, enumeratedValues.length);
+            enumeratedValues.push(enumeratedValue);
+            Object.defineProperty (createdEnum, name, { value: enumeratedValue, enumerable: true });
+        }
+
+        // save the names and values independently
+        Object.defineProperty (createdEnum, "names", { value: names });
+        Object.defineProperty (createdEnum, "values", { value: enumeratedValues });
+
+        return createdEnum;
+    };
+    return _;
+} ();
+
+let Player = function () {
+    let _ = Enum.create (["E", "X", "O"]);
+
+    _.next = function () {
+        const MASK = 3;
+        return _.values[this.value ^ MASK];
+    };
+
+    return _;
+} ();
+
+// Board is an abstraction on a string-based representation of a tic-tac-toe board
+let Board = function () {
+    let _ = Object.create (Base);
+
+    Object.defineProperty(_, "DIMENSION", { value: 3 });
+    Object.defineProperty(_, "SIZE", { value: _.DIMENSION * _.DIMENSION });
+
+    _.init = function (parameters) {
+
+    };
+
+    return _;
+} ();
--- src/test/js/tic-tac-toe.js
+++ src/test/js/tic-tac-toe.js
@@ -1,6 +1,44 @@
 //-----------------------------------------------------------------------------
 // test functions built on the harness
 //-----------------------------------------------------------------------------
-Test.assertTrue ("true", true);
+let testEnum = function () {
+    console.log ("testEnum");
+    let e = Enum.create (["A", "B", "C"]);
+    Test.assertTrue("A.name", e.A.name == "A");
+    Test.assertTrue("A.value", e.A.value == 0);
+
+    Test.assertTrue("A.names indexed", e[e.names[0]] == e.A);
+    Test.assertTrue("A.names", e.names[0] == e.A.name);
+    Test.assertTrue("A.values", e.values[0] == e.A);
+
+    Test.assertTrue("B.name", e.B.name == "B");
+    Test.assertTrue("B.value", e.B.value == 1);
+
+    Test.assertTrue("B.names indexed", e[e.names[1]] == e.B);
+    Test.assertTrue("B.names", e.names[1] == e.B.name);
+    Test.assertTrue("B.values", e.values[1] == e.B);
+
+    Test.assertTrue("C.name", e.C.name == "C");
+    Test.assertTrue("C.value", e.C.value == 2);
+
+    Test.assertTrue("C.names indexed", e[e.names[2]] == e.C);
+    Test.assertTrue("C.names", e.names[2] == e.C.name);
+    Test.assertTrue("C.value", e.values[2] == e.C);
+} ();
+
+let testPlayer = function () {
+    console.log ("testPlayer");
+
+    Test.assertTrue("Player.E", Player.E.value == 0);
+    Test.assertTrue("Player.X", Player.X.value == 1);
+    Test.assertTrue("Player.O", Player.O.value == 2);
+
+    Test.assertTrue("player.E.name", Player.E.name == "E");
+    Test.assertTrue("player.X.name", Player.X.name == "X");
+    Test.assertTrue("player.O.name", Player.O.name == "O");
+
+    Test.assertTrue("player.next (X -> O)", Player.X.next () === Player.O);
+    Test.assertTrue("player.next (O -> X)", Player.O.next () === Player.X);
+} ();
 
 //-----------------------------------------------------------------------------
